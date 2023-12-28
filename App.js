14f93c72d6ea1807bd67e6624eeefc9c2a417542
Lohitht909 <lohithtallapudi909@gmail.com>
2023-12-28 18:15:52 +0530

    episode4

--- App.js
+++ App.js
@@ -1,14 +1,31 @@
 import React from "react";
 import ReactDom from "react-dom/client"
 
-// const heading =( <h1 className="head" tabIndex="5">namste react using jsx</h1>)
 
-const HeadingComponent = () => {
-    return <div id="container">
-        <h1 className="heading">namaste react functional component</h1>
+const Header = () => {
+    return (
+    <div className="header">
+        <div className="logo-conatiner">  
+              <img className="logo" src="https://images.squarespace-cdn.com/content/5e473e46773e7329dde12671/07338cd3-48ea-4063-9c5e-2693f56fb192/Rooted-Food-Logo-Black-Color-Outline.jpg?format=1500w&content-type=image%2Fjpeg"/>
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About US</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
     </div>
+
+    )
+}
+
+
+const AppLayout = () =>{
+    return <div className="app"><Header/></div>
 }
 
 const root = ReactDom.createRoot(document.getElementById("root"))
 
-root.render(<HeadingComponent/>)
+root.render(<AppLayout/>)
